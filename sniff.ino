38f272ffcc492bd4fb1dcad7616280feb796507f
Fabian Salomon <git@salo.mailrange.com>
2021-05-09 00:45:04 +0200

    direct pins

--- sniff.ino
+++ sniff.ino
@@ -4,7 +4,6 @@
    https://github.com/sp0x/LcdSniffer/blob/master/lcd_receiver.ino
    https://en.wikipedia.org/wiki/Hitachi_HD44780_LCD_controller
 */
-
 #include <FastGPIO.h>
 
 #define EN 2
@@ -12,16 +11,19 @@
 #define D6 4
 #define D5 5
 #define D4 6
-#define RW 7
-#define RS 8
+#define RS 7
+#define RW 8
 #define NUM_CHARS 32
 
 volatile char d4, d5, d6, d7, rs, rw;
+
 
 void decode(void);
 
 bool gotUpNib = false;
 uint8_t upperNibble;
+uint8_t ;
+
 long changetime = 0;
 
 char LCD[NUM_CHARS] = { 32 };
@@ -30,19 +32,20 @@ uint8_t charIndex = 0;
 
 void setup() {
   Serial.begin(115200);
-  FastGPIO::Pin<RS>::setInput();    //Define RS
-  FastGPIO::Pin<RW>::setInput();    //Define R/W
-  FastGPIO::Pin<D7>::setInput();
-  FastGPIO::Pin<D6>::setInput();
-  FastGPIO::Pin<D5>::setInput();
-  FastGPIO::Pin<D4>::setInput();
+  pinMode(RS, INPUT);
+  pinMode(RW, INPUT);
+  pinMode(D7, INPUT);
+  pinMode(D6, INPUT);
+  pinMode(D5, INPUT);
+  pinMode(D4, INPUT);
   Serial.println("Ready");
 
-  attachInterrupt(digitalPinToInterrupt(EN), decode, RISING);
+  attachInterrupt(digitalPinToInterrupt(EN), decode, FALLING);
 }
 
 void loop() {
   delay(100);
+
   for (int i = 0; i < NUM_CHARS; i++) {
     if (LCD[i] != LCD_shadow[i]) {
       changetime = millis();
@@ -51,7 +54,6 @@ void loop() {
   }
 
   if (changetime > 0 && changetime < millis() - 500) {
-    Serial.println();
     for (int i = 0; i < NUM_CHARS; i++) {
       Serial.print(LCD_shadow[i]);
       if ((i + 1) % (NUM_CHARS / 2) == 0) {
@@ -63,12 +65,13 @@ void loop() {
 }
 
 inline uint8_t readBytes() {
-  rs = FastGPIO::Pin<RS>::isInputHigh() ? 1 : 0;
-  rw = FastGPIO::Pin<RW>::isInputHigh() ? 1 : 0;
-  d7 = FastGPIO::Pin<D7>::isInputHigh() ? 1 : 0;
-  d6 = FastGPIO::Pin<D6>::isInputHigh() ? 1 : 0;
-  d5 = FastGPIO::Pin<D5>::isInputHigh() ? 1 : 0;
-  d4 = FastGPIO::Pin<D4>::isInputHigh() ? 1 : 0;
+  uint8_t data = PIND;
+  rs = data & digitalPinToBitMask(RS) ? 1 : 0;
+  d4 = data & digitalPinToBitMask(D4) ? 1 : 0;
+  d5 = data & digitalPinToBitMask(D5) ? 1 : 0;
+  d6 = data & digitalPinToBitMask(D6) ? 1 : 0;
+  d7 = data & digitalPinToBitMask(D7) ? 1 : 0;
+
   return (d7 << 3) + (d6 << 2) + (d5 << 1) + d4;
 }
 
@@ -108,6 +111,9 @@ const uint8_t LCD_CLEAR_MASK = 0xFF;
 const uint8_t LCD_SETDDRAMADDR = 0x80;
 const uint8_t LCD_SETDDRAMADDR_MASK = 0x80;
 
+const uint8_t LCD_SETFUNCTION = 0x40;
+const uint8_t LCD_SETFUNCTION_MASK = 0xC0;
+
 void evalCommand(uint8_t command) {
   if ((command & LCD_CLEAR_MASK) == LCD_CLEAR) {
     for (int i = 0; i < 32; i++) {
@@ -122,6 +128,11 @@ void evalCommand(uint8_t command) {
     } else {
       charIndex = (command & 0x7f) - 48;
     }
+    return;
+  }
+  if ((command & LCD_SETFUNCTION_MASK) == LCD_SETFUNCTION) {
+    gotUpNib = false;
+    Serial.println("init");
     return;
   }
 }
