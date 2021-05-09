4544f17f38dc7cd1e65607c7ff6b0b9aa7ad625a
Fabian Salomon <git@salo.mailrange.com>
2021-05-09 02:11:45 +0200

    8-bit mode

--- sniff.ino
+++ sniff.ino
@@ -4,26 +4,29 @@
    https://github.com/sp0x/LcdSniffer/blob/master/lcd_receiver.ino
    https://en.wikipedia.org/wiki/Hitachi_HD44780_LCD_controller
 */
+
 #include <FastGPIO.h>
 
 #define EN 2
-#define D7 3
-#define D6 4
-#define D5 5
-#define D4 6
-#define RS 7
-#define RW 8
+#define RW 3
+#define RS 4
+#define D0 5
+#define D1 6
+#define D2 7
+#define D3 8
+#define D4 9
+#define D5 10
+#define D6 11
+#define D7 12
 #define NUM_CHARS 32
 
-volatile char d4, d5, d6, d7, rs, rw;
-
+volatile char d0, d1, d2, d3, d4, d5, d6, d7, rs, rw;
 
 void decode(void);
 
-bool gotUpNib = false;
+bool gotUpNib = true;
+bool fourBitMode = true;
 uint8_t upperNibble;
-uint8_t ;
-
 long changetime = 0;
 
 char LCD[NUM_CHARS] = { 32 };
@@ -32,20 +35,23 @@ uint8_t charIndex = 0;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(RS, INPUT);
-  pinMode(RW, INPUT);
-  pinMode(D7, INPUT);
-  pinMode(D6, INPUT);
-  pinMode(D5, INPUT);
-  pinMode(D4, INPUT);
+  FastGPIO::Pin<RS>::setInput();    //Define RS
+  FastGPIO::Pin<RW>::setInput();    //Define R/W
+  FastGPIO::Pin<D7>::setInput();
+  FastGPIO::Pin<D6>::setInput();
+  FastGPIO::Pin<D5>::setInput();
+  FastGPIO::Pin<D4>::setInput();
+  FastGPIO::Pin<D3>::setInput();
+  FastGPIO::Pin<D2>::setInput();
+  FastGPIO::Pin<D1>::setInput();
+  FastGPIO::Pin<D0>::setInput();
   Serial.println("Ready");
 
-  attachInterrupt(digitalPinToInterrupt(EN), decode, FALLING);
+  attachInterrupt(digitalPinToInterrupt(EN), decode, RISING);
 }
 
 void loop() {
   delay(100);
-
   for (int i = 0; i < NUM_CHARS; i++) {
     if (LCD[i] != LCD_shadow[i]) {
       changetime = millis();
@@ -54,6 +60,7 @@ void loop() {
   }
 
   if (changetime > 0 && changetime < millis() - 500) {
+    Serial.println();
     for (int i = 0; i < NUM_CHARS; i++) {
       Serial.print(LCD_shadow[i]);
       if ((i + 1) % (NUM_CHARS / 2) == 0) {
@@ -65,42 +72,46 @@ void loop() {
 }
 
 inline uint8_t readBytes() {
-  uint8_t data = PIND;
-  rs = data & digitalPinToBitMask(RS) ? 1 : 0;
-  d4 = data & digitalPinToBitMask(D4) ? 1 : 0;
-  d5 = data & digitalPinToBitMask(D5) ? 1 : 0;
-  d6 = data & digitalPinToBitMask(D6) ? 1 : 0;
-  d7 = data & digitalPinToBitMask(D7) ? 1 : 0;
-
-  return (d7 << 3) + (d6 << 2) + (d5 << 1) + d4;
+  rs = FastGPIO::Pin<RS>::isInputHigh() ? 1 : 0;
+  rw = FastGPIO::Pin<RW>::isInputHigh() ? 1 : 0;
+  d7 = FastGPIO::Pin<D7>::isInputHigh() ? 1 : 0;
+  d6 = FastGPIO::Pin<D6>::isInputHigh() ? 1 : 0;
+  d5 = FastGPIO::Pin<D5>::isInputHigh() ? 1 : 0;
+  d4 = FastGPIO::Pin<D4>::isInputHigh() ? 1 : 0;
+  if (fourBitMode) {
+    return (d7 << 3) + (d6 << 2) + (d5 << 1) + d4;
+  } else {
+    d3 = FastGPIO::Pin<D3>::isInputHigh() ? 1 : 0;
+    d2 = FastGPIO::Pin<D2>::isInputHigh() ? 1 : 0;
+    d1 = FastGPIO::Pin<D1>::isInputHigh() ? 1 : 0;
+    d0 = FastGPIO::Pin<D0>::isInputHigh() ? 1 : 0;
+    return (d7 << 7) + (d6 << 6) + (d5 << 5) + (d4 << 4) + (d3 << 3) + (d2 << 2) + (d1 << 1) + d0;
+  }
 }
 
 void decode() {
-  uint8_t nibble = readBytes();
+  if (rw == 0) {
+    // this is a write operation
+    uint8_t data = readBytes();
 
-  if (rs == 1) {
-    // print character received
-    if (gotUpNib == false) {
-      upperNibble = nibble;
-      gotUpNib = true;
-      return;
-    } else {
-      gotUpNib = false;
-      LCD[charIndex] = (char) (upperNibble << 4 | nibble);
-      //Serial.println( LCD[charIndex]);
-      charIndex = (charIndex + 1) % 32;
-      return;
+    if (fourBitMode) {
+      if (gotUpNib) {
+        data = (upperNibble << 4 | data);
+        gotUpNib = false;
+      } else {
+        upperNibble = data;
+        gotUpNib = true;
+        return;
+      }
     }
-  } else {
-    // command received
-    if (gotUpNib == false) {
-      upperNibble = nibble;
-      gotUpNib = true;
-      return;
+
+    if (rs == 1) {
+      // print character received
+      LCD[charIndex] = (char) data;
+      charIndex = (charIndex + 1) % 32;
     } else {
-      gotUpNib = false;
-      evalCommand(upperNibble << 4 | nibble);
-      return;
+      // command received
+      evalCommand(data);
     }
   }
 }
@@ -111,8 +122,8 @@ const uint8_t LCD_CLEAR_MASK = 0xFF;
 const uint8_t LCD_SETDDRAMADDR = 0x80;
 const uint8_t LCD_SETDDRAMADDR_MASK = 0x80;
 
-const uint8_t LCD_SETFUNCTION = 0x40;
-const uint8_t LCD_SETFUNCTION_MASK = 0xC0;
+const uint8_t LCD_SETFUNCTION_4BIT = 0x20;
+const uint8_t LCD_SETFUNCTION_MASK = 0x30;
 
 void evalCommand(uint8_t command) {
   if ((command & LCD_CLEAR_MASK) == LCD_CLEAR) {
@@ -130,9 +141,13 @@ void evalCommand(uint8_t command) {
     }
     return;
   }
-  if ((command & LCD_SETFUNCTION_MASK) == LCD_SETFUNCTION) {
+  if ((command & LCD_SETFUNCTION_MASK) == LCD_SETFUNCTION_MASK) {
+    fourBitMode = false;
+    return;
+  }
+  if ((command & LCD_SETFUNCTION_MASK) == LCD_SETFUNCTION_4BIT) {
+    fourBitMode = true;
     gotUpNib = false;
-    Serial.println("init");
     return;
   }
 }
